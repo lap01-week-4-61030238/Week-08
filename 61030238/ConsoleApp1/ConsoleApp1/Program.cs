04e85e7238120da0766d33464d2b8593075ead8b
“Saya” <61030238@kmitl.ac.th>
2019-11-02 19:51:35 +0700

    3

--- 61030238/ConsoleApp1/ConsoleApp1/Program.cs
+++ 61030238/ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,16 +10,25 @@ namespace ConsoleAppArray
     {
         static void Main(string[] args)
         {
+            int b;
             int[] data = new int[10];
-            data[-1] = 0;
+            data[0] = 0;
             data[1] = 1;
+            data[2] = 2;
+            data[3] = 3;
+            data[4] = 4;
+            data[5] = 5;
+            data[6] = 6;
+            data[7] = 7;
+            data[8] = 8;
             data[9] = 9;
-            data[10] = 10;
-            Console.WriteLine(data[-1]);
-            Console.WriteLine(data[1]);
-            Console.WriteLine(data[9]);
-            Console.WriteLine(data[10]);
+            b = data[4] + data[9];
+            Console.WriteLine(b);
+
+
+
             Console.ReadLine();
+
 
 
         }
