e19acc0f621028a4793c6e04a67431c41c86c000
“Saya” <61030238@kmitl.ac.th>
2019-11-02 19:32:15 +0700

    2

---
+++ 61030238/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = new int[10];
+            data[-1] = 0;
+            data[1] = 1;
+            data[9] = 9;
+            data[10] = 10;
+            Console.WriteLine(data[-1]);
+            Console.WriteLine(data[1]);
+            Console.WriteLine(data[9]);
+            Console.WriteLine(data[10]);
+            Console.ReadLine();
+        }
+
+    }
+}
+
+
