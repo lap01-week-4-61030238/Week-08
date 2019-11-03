68112c216df2db8092ff3a1818a916184afa52c3
“Saya” <61030238@kmitl.ac.th>
2019-11-03 19:42:47 +0700

    11

--- 61030238/ConsoleApp1/ConsoleApp1/Program.cs
+++ 61030238/ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,24 +12,26 @@ namespace ConsoleAppArray
         {
             int p;
             Console.WriteLine("ป้อนค่าที่1แต่ละเดือน12เดือน");
+            Console.WriteLine("ป้อนเลข1=วันจันทร์ม2=อังคาร,3=พุธ,4=พฤหัส,5=ศุกร์,6=เสาร์,7=อาทิตย์");
             int[] gpa = new int[12];
-            for (int n=0;n<12;n++)
+            for (int n = 0; n < 12; n++)
             {
-                int  x;
-                
+                int x;
+
                 x = int.Parse(Console.ReadLine());
                 gpa[n] = x;
             }
+            Console.WriteLine("ป้อนเดือน");
             for (int n = 0; n < 8; n++)
             {
                 Console.WriteLine("เกรด gps ");
                 Console.WriteLine(gpa[n]);
 
             }
-            p = (gpa[0] + gpa[1] + gpa[2]+ gpa[3] + gpa[4] + gpa[5] + gpa[6] + gpa[7])/8;
-            Console.WriteLine("เกรด gpa ");
-            Console.WriteLine(p);
+            switch
+        {
             Console.ReadLine();
+        }
 
 
 
