38d11efd3a0c21f5aa821a3ae68af628f7fcdd95
“Saya” <61030238@kmitl.ac.th>
2019-11-04 10:14:47 +0700

    19

--- 61030238/ConsoleApp1/ConsoleApp1/Program.cs
+++ 61030238/ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,15 +10,7 @@ namespace ConsoleAppArray
     {
         static void Main(string[] args)
         {
-            // Array of characters.
-            char[] array1 = { 'q', 'w', 'e', 'r', 't',
-                'y', 'u', 'i', 'o', 'p' };
-            Array.Sort(array1);
-
-            // Array of strings.
-            string[] colors = new string[] { "red",
-              "orange", "blue", "green", "yellow", "lemon", "aqua"  };
-            Array.Sort(colors);
+           
 
             string[] AseanCountries = new string[] { "Cambodia", "Malaysia",
               "Indonesia", "Singapore", "Thailand", "Philippines",
@@ -32,14 +24,7 @@ namespace ConsoleAppArray
                                  orderby country descending
                                  select country;
 
-            // print output
-            Console.WriteLine("------Character sorting----------");
-            foreach (var c in array1)
-                Console.WriteLine(c);
-
-            Console.WriteLine("------String sorting----------");
-            foreach (string color in colors)
-                Console.WriteLine(color);
+           
 
             Console.WriteLine("------String sort ascending----------");
             foreach (string c in sortAscending)
