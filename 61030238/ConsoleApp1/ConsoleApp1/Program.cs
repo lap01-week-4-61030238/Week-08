02c173bdf4473d497ec03d76a61832b287bfe803
“Saya” <61030238@kmitl.ac.th>
2019-11-04 18:49:51 +0700

    31

--- 61030238/ConsoleApp1/ConsoleApp1/Program.cs
+++ 61030238/ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,23 +10,32 @@ namespace ConsoleAppArray
     {
         static void Main(string[] args)
         {
-            int q = 0;
-            int[,] multiplyTable = new int[12, 12];
-            for (int N = 0; N < 12; N = N + 1)
+            int[][] jagged = new int[7][];
+
+            int count = 0;
+            for (int row = 0; row < jagged.GetLength(0); ++row)
             {
-                q++;
-                multiplyTable[N, 0] = 2 * q;
-                
-            }
-            for (int row = 0; row < multiplyTable.GetLength(0); row++)
-            {
-                for (int col = 0; col < multiplyTable.GetLength(1); col++)
+                Console.Write("\nRow {0}:", row);
+                jagged[row] = new int[row + 1];
+                for (int index = 0; index < row + 1; ++index)
                 {
-                    Console.Write("{0,5}", multiplyTable[row, col]);
+                    ++count;
+                    jagged[row][index] = count;
+                    Console.Write(" {0}", count);
                 }
-                Console.WriteLine();
+            }
+            Console.WriteLine("\n\nTotals");
+            for (int row = 0; row < jagged.GetLength(0); ++row)
+            {
+                int total = 0;
+                for (int index = 0; index < jagged[row].GetLength(0); ++index)
+                {
+                    total += jagged[row][index];
+                }
+                Console.Write("\nRow {0}: {1}", row, total);
             }
             Console.ReadLine();
+
 
 
         }
